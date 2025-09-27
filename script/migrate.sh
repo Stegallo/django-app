3a4e864f6ed6887c5c2f041d2c4a4535abbd13e4
Stegallo <stegallo@gmail.com>
2025-09-27 09:54:33 -0700

    fix static files

--- script/migrate.sh
+++ script/migrate.sh
@@ -1,3 +1,5 @@
 uv run manage.py makemigrations
 uv run manage.py migrate
-uv run manage.py createsuperuser
+# https://stackoverflow.com/questions/76985252/pythonanywhere-admin-site-css-is-broken-in-django
+uv run manage.py collectstatic
+# uv run manage.py createsuperuser
